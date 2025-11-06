6ac90ca0ae979a1325e41a90c1d90d612d70a2f7
Keshav Vishwakarma <vishwakarmakeshav22@gmail.com>
2025-11-06 22:07:57 +0530

    prod

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 import eventlet
 eventlet.monkey_patch()
 
-from app import app, socketio
 import os
+os.environ.setdefault('FLASK_ENV', 'production')
+
+from app import app
 
 # This is what Gunicorn will import
 application = app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 8085))
+    app.run(host="0.0.0.0", port=port, debug=False)
